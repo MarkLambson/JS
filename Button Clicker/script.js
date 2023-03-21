34d0d247a6c8c5381e3a8ebcd410040aeb04c01e
MarkLambson <mj.lambson@yahoo.com>
2023-03-21 11:59:52 -0700

    button clicker

---
+++ Button Clicker/script.js
@@ -0,0 +1,12 @@
+function remove() {
+    var element = this.id;
+    element.remove();
+}
+
+function change_text() {
+    document.getElementById("loginbutton").innerHTML = "Logout";
+}
+
+function addLike() {
+    alert("Ninja was liked");
+}
